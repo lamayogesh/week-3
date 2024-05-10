e59e38554261c03f2bb9f70e112ccc1f3a79c49a
lamayogesh <53514764+lamayogesh@users.noreply.github.com>
2024-05-10 13:44:12 +0545

    Update mongooseDemo.js
    Created user signin that store data in database.

--- mongooseDemo.js
+++ mongooseDemo.js
@@ -1,4 +1,8 @@
+const express = require("express");
 const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
 
 mongoose.connect(
   "mongodb+srv://admin:OINyd1Ur6lAtUSWA@cluster0.atwetyt.mongodb.net/userappnew",
@@ -10,11 +14,27 @@ const User = mongoose.model("Users", {
   password: String,
 });
 
-const user = new User({
-  name: "yogesh",
-  email: "yogesh@gmail.com",
-  password: "123456",
+app.post("/signin",async function (req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const existingUser = await User.findOne({ email: email });
+
+  if (existingUser) {
+    return res.status(400).send("User already exists");
+  }
+
+  const user = new User({
+    name: name,
+    email: email,
+    password: password,
+  });
+
+  user.save();
+  res.json({
+    mesage: "User created successfully",
+  });
 });
 
-
-user.save();
+app.listen(3000);
